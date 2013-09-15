fb3da747280d7e5389d532f28b34504facd3c628
Peter Law <PeterJCLaw@gmail.com>
2013-09-15 15:24:09 +0100

    Expose the team & college prefixes to clients.
    Change-Id: I3ae5a6e1d74c89da6e40057d42862ab84533600e

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 import groups, users, sr_ldap
 from users import user, new_username
-from groups import group
+from groups import group, TEAM_PREFIX, COLLEGE_PREFIX
 from sr_ldap import set_userinfo
